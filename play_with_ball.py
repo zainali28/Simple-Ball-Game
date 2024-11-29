463fc48fa134a7d054cc08f70e8dfa975d2879d1
zainali28 <zainali123113@gmail.com>
2024-11-29 17:26:40 +0500

    added new features

--- play_with_ball.py
+++ play_with_ball.py
@@ -1,35 +1,69 @@
 import pygame
 
+# initializing pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+display_size = (1280, 720)
+
+# setting display size
+screen = pygame.display.set_mode(display_size)
+
+# initializing clock module
 clock = pygame.time.Clock()
+
+# setting initial state of game to start the main game loop
 running = True
+
+# initialize the change in time for the physics in game
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# ball radius
+radius = 40
+
+# jump action default state
+jump = 0
+
+# default position of the circle
+player_pos = pygame.Vector2(radius+5, screen.get_height()-radius-5)
 
 while running:
-
+    
+    # checking if the game is running or quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # filling screen with purple color
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    
+    # drawing a circle at center of screem
+    pygame.draw.circle(screen, "red", player_pos, radius)
 
+    # handling key events and binding them to specific actions
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        if player_pos.y > (radius + 5):
+            player_pos.y -= 300 * dt 
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        if player_pos.y < (display_size[1]-radius-5):
+            player_pos.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        if player_pos.x > (radius + 5):
+            player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        if player_pos.x < (display_size[0]-radius-5):
+            player_pos.x += 300 * dt
+    if keys[pygame.K_SPACE]:
+        jump = 1
 
+    # showing everything that we have done above on display
     pygame.display.flip()
 
+    # converting from ms to s
+    # for every 1s at most 60 frames should pass
+    # returns number of ms passed since its last call
     dt = clock.tick(60) / 1000
 
+# quit game once user closes the window
 pygame.quit()
